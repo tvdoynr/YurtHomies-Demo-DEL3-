2254a765a89530112ea2f84ac2570515d0ceb315
Abdullah Mert Dinçer <b2200356016@cs.hacettepe.edu.tr>
2023-04-26 19:32:29 +0300

    initial

---
+++ src/Login.jsx
@@ -0,0 +1,25 @@
+import React, { useState } from "react";
+
+export const Login = (props) => {
+    const [id, setID] = useState('');
+    const [pass, setPass] = useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(id);
+    }
+
+    return (
+        <div className="auth-form-container">
+            <h2>Login</h2>
+            <form className="login-form" onSubmit={handleSubmit}>
+                <label htmlFor="id">Student ID</label>
+                <input value={id} onChange={(e) => setID(e.target.value)} placeholder="Student ID" id="id" name="id" />
+                <label htmlFor="password">Password</label>
+                <input value={pass} onChange={(e) => setPass(e.target.value)} type="password" placeholder="Your Password" id="password" name="password" />
+                <button type="submit">Log In</button>
+            </form>
+            <button className="link-btn" onClick={() => props.onFormSwitch('register')}>Don't have an account? Enroll here.</button>
+        </div>
+    )
+}
---
+++ src/Register.jsx
@@ -0,0 +1,25 @@
+import React, { useState } from "react";
+
+export const Register = (props) => {
+    const [name, setName] = useState('');
+    const [id, setID] = useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(id);
+    }
+
+    return (
+        <div className="auth-form-container">
+            <h2>Enroll</h2>
+            <form className="register-form" onSubmit={handleSubmit}>
+                <label htmlFor="name">Full name</label>
+                <input value={name} name="name" onChange={(e) => setName(e.target.value)} id="name" placeholder="Full Name" />
+                <label htmlFor="id">Student ID</label>
+                <input value={id} onChange={(e) => setID(e.target.value)} placeholder="Student ID" id="id" name="id" />
+                <button type="submit">Request to Enroll</button>
+            </form>
+            <button className="link-btn" onClick={() => props.onFormSwitch('login')}>Already enrolled? Sign in now.</button>
+        </div>
+    )
+}
